574acb57c05a92436e3c2455daa4e6963fedff6d
Anurag rathi <rathisanjita32@gmail.com>
2024-12-29 14:36:13 +0530

    this is the new blog project based on MERN stack
    This includes working of various operation like conditional rendering, jasonwebtoken, authentication for protected patch ,mongodb etc...

---
+++ FullStack/BlogApp/Server/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = 9000;
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+app.listen(PORT, () => {
+    console.log(`API is running on http://localhost:${PORT}`);
+});
